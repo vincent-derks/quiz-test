b48c802dbef5f2daf5373f03b0456f942f9ba61e
V Derks <vderks@Vs-iMac.local>
2017-11-20 00:04:00 +0100

    Added webpack/babel config for server-side code

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,32 @@
-module.exports = {
+var serverConfig = {
+    target: 'node',
+    entry: __dirname + '/src/server/index.js',
+    output: {
+        path: __dirname,
+        filename: 'src/server/compiled/bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    babelrc: false,
+                    presets: ['env', 'es2017'],
+                    plugins: ['transform-object-rest-spread', 'transform-runtime']
+                }
+            }
+        ],
+
+    }
+}
+
+var clientConfig = {
     entry: __dirname + '/src/client/index.js',
     output: {
-        path: __dirname + '/web/js',
-        filename: 'bundle.js'
+        path: __dirname,
+        filename: 'web/js/bundle.js'
     },
     module: {
         loaders: [
@@ -20,3 +44,5 @@ module.exports = {
 
     }
 }
+
+module.exports = [serverConfig, clientConfig]
